ef290c10449e20c5b3b586ead9c051a0188654d6
saravahdatipour <verpiderp@googlemail.com>
2024-05-11 22:07:37 +0200

    pass bool to ftl

--- extensions/extension-disclosure-authenticator/src/main/java/com/inventage/keycloak/disclosureauthenticator/infrastructure/authenticator/DisclosureAuthenticator.java
+++ extensions/extension-disclosure-authenticator/src/main/java/com/inventage/keycloak/disclosureauthenticator/infrastructure/authenticator/DisclosureAuthenticator.java
@@ -10,6 +10,7 @@ import org.keycloak.credential.CredentialProvider;
 import org.keycloak.credential.PasswordCredentialProvider;
 import org.keycloak.credential.UserCredentialStore;
 import org.keycloak.events.Errors;
+import org.keycloak.forms.login.LoginFormsProvider;
 import org.keycloak.jose.jwk.JWK;
 import org.keycloak.models.*;
 import org.keycloak.models.credential.PasswordCredentialModel;
@@ -25,28 +26,48 @@ import org.keycloak.services.messages.Messages;
 import org.keycloak.services.validation.Validation;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class DisclosureAuthenticator implements Authenticator  {
     private static final Logger LOG = Logger.getLogger(DisclosureAuthenticator.class);
-    public static final String DISCLOSURE_FORM = "disclosure-form.ftl";
+    public static final String USERNAME_PASSWORD_LOGIN_FORM = "username-password-login.ftl";
+    public static final String YIVI_FORM = "backup.ftl";
+    public static boolean YIVI_TOGGLE = true;
 
 
     // TODO: write authenticate logic
     @Override
+//    public void authenticate(AuthenticationFlowContext context) {
+//        // 1. This method is called first.
+//        // It checks if the current request meets the requirements of the authenticator.
+//        // If not, it may prompt the user for additional information or actions.
+//        LOG.warnf("authenticate");
+//        boolean enableSecondOption = Boolean.parseBoolean(context.getAuthenticatorConfig().getConfig().get("enableSecondOption"));
+//        boolean over18 = Boolean.parseBoolean(context.getAuthenticatorConfig().getConfig().get("over18property"));
+//        boolean yiviEmailEnabled = Boolean.parseBoolean(context.getAuthenticatorConfig().getConfig().get("yiviemail"));
+//        context.getAuthenticationSession().setAuthNote("yiviEmailEnabled", String.valueOf(yiviEmailEnabled));
+//
+//
+//        LOG.warnf(String.valueOf(yiviEmailEnabled));
+//        LOG.warnf("configuration working");
+//
+//
+//        Response challengeusernamepassword = context.form().createForm(USERNAME_PASSWORD_LOGIN_FORM);
+//        context.challenge(challengeusernamepassword);
+//
+//    }
     public void authenticate(AuthenticationFlowContext context) {
-        // 1. This method is called first.
-        // It checks if the current request meets the requirements of the authenticator.
-        // If not, it may prompt the user for additional information or actions.
-        LOG.warnf("authenticate");
-
-
-        Response challenge = context.form()
-
-                .createForm(DISCLOSURE_FORM);
-
-        context.challenge(challenge);
-
+        LOG.debugf("authenticate");
+        final LoginFormsProvider formsProvider = context.form();
+        formsProvider.setAttribute("formModel", new DisclosureFormModel());
+        // Example boolean variable
+        boolean isUserVerified = true;
+        formsProvider.setAttribute("isUserVerified", isUserVerified);
+        context.challenge(formsProvider.createForm("disclosure.ftl"));
     }
 
 
@@ -75,6 +96,11 @@ public class DisclosureAuthenticator implements Authenticator  {
 
     }
 
+    public void authenticateWithYivi(AuthenticationFlowContext context){
+
+        //has logic for redirecting to yivi and translating a response to credentials keycloak understands
+    }
+
     @Override
     public void action(AuthenticationFlowContext context) {
         LOG.warnf("action");
@@ -88,7 +114,7 @@ public class DisclosureAuthenticator implements Authenticator  {
             //Form is empty somewhere
             context.getEvent().error("Username is missing");
             Response challenge = context.form()
-                    .createForm(DISCLOSURE_FORM);
+                    .createForm(USERNAME_PASSWORD_LOGIN_FORM);
 
             context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
             return;
@@ -101,7 +127,7 @@ public class DisclosureAuthenticator implements Authenticator  {
             context.getEvent().error(Errors.USER_NOT_FOUND);
             Response challenge = context.form()
                     .setError(Messages.INVALID_USER)
-                    .createForm(DISCLOSURE_FORM);
+                    .createForm(USERNAME_PASSWORD_LOGIN_FORM);
             context.failureChallenge(AuthenticationFlowError.INVALID_USER, challenge);
             return;
         }
@@ -117,7 +143,7 @@ public class DisclosureAuthenticator implements Authenticator  {
             context.getEvent().user(user).error(Errors.INVALID_USER_CREDENTIALS);
             Response challenge = context.form()
                     .setError(Messages.INVALID_PASSWORD)
-                    .createForm(DISCLOSURE_FORM);
+                    .createForm(USERNAME_PASSWORD_LOGIN_FORM);
             context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challenge);
         }
 
--- extensions/extension-disclosure-authenticator/src/main/java/com/inventage/keycloak/disclosureauthenticator/infrastructure/authenticator/DisclosureAuthenticatorFactory.java
+++ extensions/extension-disclosure-authenticator/src/main/java/com/inventage/keycloak/disclosureauthenticator/infrastructure/authenticator/DisclosureAuthenticatorFactory.java
@@ -3,11 +3,13 @@ package com.inventage.keycloak.disclosureauthenticator.infrastructure.authentica
 import org.keycloak.Config;
 import org.keycloak.authentication.Authenticator;
 import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.broker.social.SocialIdentityProvider;
 import org.keycloak.models.AuthenticationExecutionModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
 import org.keycloak.provider.ProviderConfigProperty;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,7 +34,7 @@ public class DisclosureAuthenticatorFactory implements AuthenticatorFactory {
 
     @Override
     public boolean isConfigurable() {
-        return false;
+        return true;
     }
 
     @Override
@@ -51,10 +53,36 @@ public class DisclosureAuthenticatorFactory implements AuthenticatorFactory {
     public String getHelpText() {
         return "This Authenticator does nothing for now. but Disclosure will be implemented soon";
     }
-
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
-        return Collections.emptyList();
+        List<ProviderConfigProperty> configProperties = new ArrayList<>();
+        ProviderConfigProperty enable_property= new ProviderConfigProperty();
+        enable_property.setName("enableSecondOption");
+        enable_property.setLabel("Enable Second Option");
+        enable_property.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        enable_property.setDefaultValue("false");
+        enable_property.setHelpText("Enable or disable using Yivi.");
+        configProperties.add(enable_property);
+
+        ProviderConfigProperty over_18= new ProviderConfigProperty();
+        over_18.setName("over18property");
+        over_18.setLabel("over18 property");
+        over_18.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        over_18.setDefaultValue("false");
+        over_18.setHelpText("Enable or disable using over 18 credential.");
+        configProperties.add(over_18);
+
+        ProviderConfigProperty yivi_email= new ProviderConfigProperty();
+        yivi_email.setName("yiviemail");
+        yivi_email.setLabel("yivi email property");
+        yivi_email.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        yivi_email.setHelpText("Enable or disable using email credential");
+        configProperties.add(yivi_email);
+
+
+
+
+        return configProperties;
     }
 
     @Override
